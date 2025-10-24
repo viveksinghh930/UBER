a7a9aded860d5a3974b9279da1762eb07229b51d
Vivek Singh Tomar <tomarvivek669@gmail.com>
2025-10-24 13:38:50 +0530

    user aurhentication done

--- Backend/app.js
+++ Backend/app.js
@@ -6,12 +6,14 @@ const express = require("express")
 const app = express();
 const connectToDb = require("./DB/db")
 const userRoutes = require("./routes/user.routes")
+const cookieParser= require("cookie-parser")
 
 connectToDb();
  
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.get("/",(req , res)=>{
 res.send("hellow world")
